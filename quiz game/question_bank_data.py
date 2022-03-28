d41127e91b633b1bcbfa3a4ce5f61b33eb8067bc
Kwilso3412 <80131328+Kwilso3412@users.noreply.github.com>
2022-03-27 21:05:55 -0400

    Update question_bank_data.py
    changed the dictionary to a list

--- quiz game/question_bank_data.py
+++ quiz game/question_bank_data.py
@@ -1,163 +1,163 @@
 #This dictionary is used to make a question bank for the dad joke quiz game
 
 #"question" is the key and "answer" is the value
-question_bank = { 
-    "question": "What do sprinters eat before a race?", 
-    "multiple choice": "A)",
-    "answer":"nothing they fast", 
-    "hint": "what is the religious practice of not eating"
-    }
+question_bank = ["What do sprinters eat before a race?", #question
+    "A) Celery B) A protein shake C) Carrots D) Nothing they fast ", #multiple choice
+    "D", #answer
+    "what is the religious practice of not eating" #Hint
+    ] 
 
-{
-    "question": "Air use to be free at the gas station, now its a $1.50. You know why?", 
-    "multiple choice": "A)",
-    "answer": "inflation", 
-    "hint":"the opposite of deflation"
-}
 
-{
-    "question": "How did the man on the moon cut his hair?", 
-    "multiple choice": "A)",
-    "answer": "he eclipse it",
-    "hint":"what is it called when the sun covers the moon"
-} 
+[
+    "Air use to be free at the gas station, now its a $1.50. You know why?", #question
+    "A) Hard times B) Inflation C) Greedy Gas station owners D) The trying times of 1856 ", #multiple choice
+    "B", #answer
+    "the opposite of deflation"#Hint
+]
 
-{
-    "question": "What do a tick and the Eiffel Tower have in common?", 
-    "multiple choice": "A)",
-    "answer":"They're both Paris sites", 
-    "hint":"where is the Eiffel tower located"
-}
+[
+    "How did the man on the moon cut his hair?", #question
+    "A) Scissors B) He eclipse it  C) He brought his barber D) With some moon rocks", #multiple choice
+    "B", #answer
+    "What is it called when the sun covers the moon"#Hint
+]
 
-{
-    "question": "What do you call a fish wearing a bowtie?",
-    "multiple choice": "A)",
-    "answer": "Sofishticated", 
-    "hint":"what is a word for a knowledgeable fashionable person"
-}
+[
+    "What do a tick and the Eiffel Tower have in common?", 
+    "A) They're both Paris sites B) They like to stay in one place C) People have to travel to find them D) Both are costly, one to see and one if you don't remove",
+    "A", 
+    "Where is the Eiffel tower located"
+]
 
-{
-    "question": "If April showers bring May flowers, what do May flowers bring?", 
-    "multiple choice": "A)",
-    "answer": "Pilgrims", 
-    "hint":"who sailed to the united states"
-}
+[
+    "What do you call a fish wearing a bowtie?",
+    "A) A werido B) A confused fish  C) Sofishticated D) A weird flex ",
+    "C", 
+    "What is a word for a knowledgeable fashionable person"
+]
 
-{
-    "question": "What do you call a factory that makes okay products?", 
-    "multiple choice": "A)",
-    "answer":"A satisfactory", 
-    "hint":"what is another way to to say you did a good job"
-}
+[
+    "If April showers bring May flowers, what do May flowers bring?", 
+    "A) Instagram models B) Pilgrims C) Bees D) Pollen season",
+    "B", 
+    "Who sailed to the united states"
+]
 
-{
-    "question": "What did the janitor say when he jumped out of the closet?", 
-    "multiple choice": "A)",
-    "answer":"Supplies", 
-    "hint":"____ & demand"
-}
+[
+    "What do you call a factory that makes okay products?", 
+    "A) A dollar store supplier B) A satisfactory C) Your average factory D) Walmart",
+    "B", 
+    "What is another way to to say you did a good job"
+]
 
-{
-    "question":"What did one wall say to the other?", 
-    "multiple choice": "A)",
-    "answer": "I'll meet you at the corner", 
-    "hint":"what is it called when two edges meet"
-}
+[
+    "What did the janitor say when he jumped out of the closet?", 
+    "A) Were out of bleach B) I wasn't napping! C) Who locked me in there D) Supplies!",
+    "D", 
+    "____ & demand"
+]
 
-{
-    "question":"What did the zero say to the eight?", 
-    "multiple choice": "A)",
-    "answer" :"That belt looks good on you",
-    "hint":"what do you use to hold your pants up, that is not suspenders"
-}
+[
+    "What did one wall say to the other?", 
+    "A) How many wholes are they going to put in us? B) Hey is your paint dry yet C) I'll meet you at the corner D) I don't like this new color on us",
+    "C", 
+    "What is it called when two edges meet"
+]
 
-{
-    "question": "What do you call a poor Santa Claus?",
-    "multiple choice": "A)",
-    "answer": "St. Nickel-less", 
-    "hint":"st. Nick"
-}
+[
+    "What did the zero say to the eight?", 
+    "A) Why did you let 7 eat 9 B) That belt looks good on you C) So are you just infinity standing up D) Me and one are going to make 10",
+    "B",
+    "What do you use to hold your pants up, that is not suspenders"
+]
 
-{
-    "question":"What did one hat say to the other?", 
-    "multiple choice": "A)",
-    "answer":"Stay here! I'm going on ahead", 
-    "hint":"where do hats go on a person"
-}
+[
+    "What do you call a poor Santa Claus?",
+    "A) St. Nickel-less B) A man who's job was automated C) Fired from coca-cola D) Divorced from Mrs. Claus and she got the workshop",
+    "A", 
+    "St. Nick"
+]
 
-{
-    "question": "Why was the PowerPoint presentation so desperate to cross the road?", 
-    "multiple choice": "A)",
-    "answer":"Because he badly wanted to go the other slide",
-    "hint":"what is a powerpoint presontation made of"
-}
+[
+    "What did one hat say to the other?", 
+    "A) I hope they washed their head B) Looks like he got a bad hair cut again C) Stay here! I'm going on ahead D) The owner has a lot of dandruff",
+    "C", 
+    "Where do hats go on a person"
+]
 
-{
-    "question":"Why did everyone tell the new computer teacher that he was always extremely confused on Twitter?",
-    "multiple choice": "A)",
-    "answer":"It was probably because he didn't follow", 
-    "hint":"how do you stay up to date with a page on twitter"
-}
+[
+    "Why was the PowerPoint presentation so desperate to cross the road?", 
+    "A)  B) C) D) Because he badly wanted to go the other slide",
+    "D",
+    "What is a powerpoint presontation made of"
+]
 
-{
-    "question":"What is the only reason why a car made by computer manufacturers doesn't last for a very long time?",
-    "multiple choice": "A)",
-    "answer":"It is perhaps because they have hard drives", 
-    "hint":"if your computer doesn't have a SD it has a ___"
-}
+[
+    "Why did everyone tell the new computer teacher that he was always extremely confused on Twitter?",
+    "A) All his tweets were" + '"Chirp"'+ "B) It was probably because he didn't follow C) He made weird hastags D) ",
+    "B", 
+    "How do you stay up to date with a page on twitter"
+]
 
-{
-    "question":"Why was the computer scientist very upset when his computer got affected by a virus?", 
-    "multiple choice": "A)",
-    "answer":"Because it was a terminal illness", 
-    "hint": "What is another word for console or command line"
-}
+[
+    "What is the only reason why a car made by computer manufacturers doesn't last for a very long time?",
+    "A) You shouldn't ask a fisherman to do the job of a hunter B) An engine doesn't have Ram C) It is perhaps because they have hard drives D) They don't account for oil changes",
+    "C", 
+    "if your computer doesn't have a SD it has a ___"
+]
 
-{
-    "question":"Which animal is the absolute expert in navigating the internet? A spider.", 
-    "multiple choice": "A)",
-    "answer":"Because spiders know all about the web", 
-    "hint":"what do spiders use to trap their prey"
-}
+[
+    "Why was the computer scientist very upset when his computer got affected by a virus?", 
+    "A) Because now it needed console-ing B) Because people will find out about his Malware practice C) Because it ran someware D) Because it was a terminal illness",
+    "D", 
+    "What is another word for console or command line"
+]
 
-{
-    "question":"Why did the functions stop calling each other?", 
-    "multiple choice": "A)",
-    "answer":"Because they had constant arguments",
-    "hint":"what goes inside a function"
-}
+[
+    "Why is the A spider the absolute expert in navigating the internet?", 
+    "A) Because they can type quickly B) Because they know all about the web C) Because they have some many eyes D) Because they took a comp sci class with Peter Parker",
+    "B", 
+    "What do spiders use to trap their prey"
+]
 
-{
-    "question":"What do you call a skinny ghost?",
-    "multiple choice": "A)",
-    "answer":"BOOLEAN",
-    "hint": "what are the AND, OR, NOT or AND NOT operators used for"
-}
+[
+    "Why did the functions stop calling each other?", 
+    "A) Because they had constant arguments B) Someone broke their function their call C) They had conflicting data types D) They had syntax errors",
+    "A",
+    "What goes inside a function"
+]
 
-{
-    "question":"Why does Yoda’s code always crash?", 
-    "multiple choice": "A)",
-    "answer":"Because there is no try", 
-    "hint": "do or do not"
-}
+[
+    "What do you call a skinny ghost?",
+    "A) Casper on a diet B) King Hamlets boot camp recruit C) The phantom menace D) BOOLEAN",
+    "D",
+    "What kind of operators are AND, OR, NOT or AND NOT"
+]
 
-{
-    "question":"Why do anarchists like functional programming?", 
-    "multiple choice": "A)",
-    "answer":"Because it has no state", 
-    "hint":"what are New jersey, ohio, nevada"
-}
+[
+    "Why does Yoda's code always crash?", 
+    "A) He can't type with his fat fingers B) Programing skills weren't taught in Jedi school C) Because there is no try D) ",
+    "C", 
+    "do or do not"
+]
 
-{
-    "question":"What’s the second movie about a database engineer called?",
-    "multiple choice": "A)",
-    "answer":"SQL",
-    "hint":"domain-specific language used in programming and designed for managing data "
-}
+[
+    "Why do anarchists like functional programming?", 
+    "A) Because it has no state B) Because Object oriented programing has to much structure C) There's no hierarchy D) Procedural Programming has to many rules",
+    "A", 
+    "what are New jersey, ohio, nevada"
+]
 
-{
-    "question":"Why didn't the client tip the server?",
-    "multiple choice": "A)",
-    "answer":"Because they didn't have enough cache", 
-    "hint":"something in computer science that stores data so that future requests for that data can be served faster;"
-}
+[
+    "What's the second movie about a database engineer called?",
+    "A) Boring B) Free Guy 2: Wrath of the database C) SQL D) Ex Machina 2 the attack of the database ",
+    "C",
+    "domain-specific language used in programming and designed for managing data "
+]
+
+[
+    "Why didn't the client tip the server?",
+    "A) Because they didn't have enough cache B) Their card declined C) The server gave bad service D) It was included in the bill",
+    "A", 
+    "something in computer science that stores data so that future requests for that data can be served faster;"
+]
