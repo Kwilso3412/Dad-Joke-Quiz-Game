a5c740f13d8ba63d30bfaa3c86a509bd1fbc8403
Kahlyll Wilson <Kahswdirect23@gmail.com>
2024-05-03 07:31:26 -0400

    Delete quiz game directory

--- quiz game/src/multiple_choice.py
+++
@@ -1,8 +0,0 @@
-#This class is used to create the quiz object and corresponding methods
-
-#question class is used for creating question object and corresponding methods
-class MC_Quiz:
-    def __init__(self, prompt, answer):
-        self.prompt = prompt
-        self.answer = answer
-
